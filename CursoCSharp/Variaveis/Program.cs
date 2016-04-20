7e1617f88813cb806a115d5df61f7cd8c33d6936
Wellington Schütz <wellington.schu@live.com>
2016-04-20 18:34:51 -0300

    Initial Commit

---
+++ CursoCSharp/Variaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String nomePessoa;
+
+            Console.WriteLine("Digite seu nome: ");
+            nomePessoa = Console.ReadLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
