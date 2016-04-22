edc658b18487a98b6f680aad60d8f63d790242a5
Wellington Schütz <wellington.schu@live.com>
2016-04-22 19:21:03 -0300

    Implementação Projeto while

---
+++ CursoCSharp/While/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double numero;
+            double total = 0;
+            string operacao = "+";
+
+            while(operacao != "=")
+            {
+
+                Console.WriteLine("Informe o valor: ");
+                numero = Convert.ToDouble(Console.ReadLine());
+
+                switch (operacao)
+                {
+
+                    case "+":
+
+                        total = total + numero;
+                        break;
+
+                    case "-":
+
+                        total = total - numero;;
+                        break;
+
+                    case "*":
+                        total = total * numero;;
+                        break;
+
+                    case "/":
+                        total = total / numero;
+                        break;
+
+                }
+
+            }
+
+
+        }
+    }
+}
